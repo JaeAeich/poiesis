21580b0210edbe2319702995eb8aa8aa2a4bc1aa
Javed Habib <jh4official@gmail.com>
2025-02-24 23:32:49 +0530

    feat(torc): create tof job

--- poiesis/core/services/torc/torc_tof_execution.py
+++ poiesis/core/services/torc/torc_tof_execution.py
@@ -1,42 +1,121 @@
-"""Create Tof Job and monitor it."""
+"""Create TOF Job and monitor it."""
 
+import json
+import logging
 from typing import Optional
 
+from kubernetes.client import (
+    V1Container,
+    V1Job,
+    V1JobSpec,
+    V1ObjectMeta,
+    V1PersistentVolumeClaimVolumeSource,
+    V1PodSpec,
+    V1PodTemplateSpec,
+    V1Volume,
+    V1VolumeMount,
+)
+from kubernetes.client.exceptions import ApiException
+
 from poiesis.api.tes.models import TesOutput
+from poiesis.core.constants import PoiesisCoreConstants
 from poiesis.core.services.torc.torc_execution_template import TorcExecutionTemplate
+
+logger = logging.getLogger(__name__)
 
 
 class TorcTofExecution(TorcExecutionTemplate):
-    """Tif execution class.
+    """TOF execution class.
 
     This class is responsible for creating the Tof Job.
 
     Args:
-        name: The name of the TES task will be modified for Tof Job.
+        name: The name of the TES task will be modified for TOF Job.
         outputs: The list of outputs that Tof will create and monitor.
 
     Attributes:
-        name: The name of the TES task will be modified for Tof Job.
-        outputs: The list of outputs that Tof will create and monitor.
+        name: The name of the TES task will be modified for TOF Job.
+        outputs: The list of outputs that TOF will create and monitor.
+        message_broker: Message broker client.
+        message: Message for the message broker which would to sent to TOrc.
+        kubernetes_client: Kubernetes client.
     """
 
     def __init__(
         self,
         name: str,
         outputs: Optional[list[TesOutput]],
-    ):
-        """Initialize the Tif execution class.
+    ) -> None:
+        """Initialize the TOF execution class.
 
         Args:
-            name: The name of the TES task will be modified for Tof Job.
+            name: The name of the TES task will be modified for TOF Job.
             outputs: The list of outputs that Tof will create and monitor.
 
         Attributes:
-            name: The name of the TES task will be modified for Tof Job.
-            outputs: The list of outputs that Tof will create and monitor.
+            name: The name of the TES task will be modified for TOF Job.
+            outputs: The list of outputs that TOF will create and monitor.
+            message_broker: Message broker client.
+            message: Message for the message broker which would to sent to TOrc.
+            kubernetes_client: Kubernetes client.
         """
-        pass
+        super().__init__()
+        self.name = name
+        self.outputs = outputs
 
-    def start_job(self, task_data):
-        """Create the K8s job for Tif."""
-        pass
+    async def start_job(self) -> None:
+        """Create the K8s job for Tof."""
+        tof_job_name = PoiesisCoreConstants.K8s.TOF_PREFIX + self.name
+        outputs = (
+            json.dumps([output.model_dump_json() for output in self.outputs])
+            if self.outputs
+            else "[]"
+        )
+
+        job = V1Job(
+            api_version="batch/v1",
+            kind="Job",
+            metadata=V1ObjectMeta(
+                name=tof_job_name,
+                labels={
+                    "service": PoiesisCoreConstants.K8s.TOF_PREFIX,
+                    "name": tof_job_name,
+                    "parent": f"{PoiesisCoreConstants.K8s.TORC_PREFIX}-{self.name}",
+                },
+            ),
+            spec=V1JobSpec(
+                template=V1PodTemplateSpec(
+                    spec=V1PodSpec(
+                        containers=[
+                            V1Container(
+                                name=PoiesisCoreConstants.K8s.TIF_PREFIX,
+                                image=PoiesisCoreConstants.K8s.POIESIS_IMAGE,
+                                command=["tof"],
+                                args=["--name", self.name, "--outputs", outputs],
+                                volume_mounts=[
+                                    V1VolumeMount(
+                                        name=PoiesisCoreConstants.K8s.COMMON_PVC_VOLUME_NAME,
+                                        mount_path=PoiesisCoreConstants.K8s.FILER_PVC_PATH,
+                                    )
+                                ],
+                            )
+                        ],
+                        volumes=[
+                            V1Volume(
+                                name=PoiesisCoreConstants.K8s.COMMON_PVC_VOLUME_NAME,
+                                persistent_volume_claim=V1PersistentVolumeClaimVolumeSource(
+                                    claim_name=f"{PoiesisCoreConstants.K8s.PVC_PREFIX}-{self.name}"
+                                ),
+                            )
+                        ],
+                        restart_policy="Never",
+                    ),
+                )
+            ),
+        )
+
+        try:
+            await self.kubernetes_client.create_job(job)
+        except ApiException as e:
+            logger.error(e)
+            raise
