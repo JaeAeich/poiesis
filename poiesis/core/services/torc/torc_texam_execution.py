2b8d7b62379747b0b79a667bb7f0c8f0e639dda9
Javed Habib <jh4official@gmail.com>
2025-02-24 23:17:27 +0530

    feat(torc): create texam job

--- poiesis/core/services/torc/torc_texam_execution.py
+++ poiesis/core/services/torc/torc_texam_execution.py
@@ -1,9 +1,24 @@
 """Create Texam Job and monitor it."""
 
+import json
+import logging
 from typing import Optional
 
+from kubernetes.client import (
+    V1Container,
+    V1Job,
+    V1JobSpec,
+    V1ObjectMeta,
+    V1PodSpec,
+    V1PodTemplateSpec,
+)
+from kubernetes.client.exceptions import ApiException
+
 from poiesis.api.tes.models import TesExecutor, TesResources
+from poiesis.core.constants import PoiesisCoreConstants
 from poiesis.core.services.torc.torc_execution_template import TorcExecutionTemplate
+
+logger = logging.getLogger(__name__)
 
 
 class TorcTexamExecution(TorcExecutionTemplate):
@@ -22,6 +37,9 @@ class TorcTexamExecution(TorcExecutionTemplate):
         executors: The list of executors that Texam will create and monitor.
         resources: The resources that need to be allocated for the executors.
         volumes: The list of volumes that need to be mounted to the executors.
+        kubernetes_client: Kubernetes
+        message_broker: Message broker.
+        message: Message for the message broker.
     """
 
     def __init__(
@@ -30,7 +48,7 @@ class TorcTexamExecution(TorcExecutionTemplate):
         executors: list[TesExecutor],
         resources: Optional[TesResources],
         volumes: Optional[list[str]],
-    ):
+    ) -> None:
         """Initialize the Tif execution class.
 
         Args:
@@ -44,9 +62,64 @@ class TorcTexamExecution(TorcExecutionTemplate):
             executors: The list of executors that Texam will create and monitor.
             resources: The resources that need to be allocated for the executors.
             volumes: The list of volumes that need to be mounted to the executors.
+            kubernetes_client: Kubernetes
+            message_broker: Message broker.
+            message: Message for the message broker.
         """
-        pass
+        super().__init__()
+        self.name = name
+        self.executors = executors
+        self.resources = resources
+        self.volumes = volumes
 
-    def start_job(self):
+    async def start_job(self) -> None:
         """Create the K8s job for Texam."""
-        pass
+        texam_name = f"{PoiesisCoreConstants.K8s.TEXAM_PREFIX}-{self.name}"
+        executors = json.dumps(
+            [self.executors.model_dump_json() for executor in self.executors]
+        )
+        resources = (
+            json.dumps(self.resources.model_dump_json()) if self.resources else "{}"
+        )
+        volumes = json.dumps(self.volumes) if self.volumes else "[]"
+        job = V1Job(
+            api_version="batch/v1",
+            kind="Job",
+            metadata=V1ObjectMeta(
+                name=texam_name,
+                labels={
+                    "service": PoiesisCoreConstants.K8s.TEXAM_PREFIX,
+                    "parent": f"{PoiesisCoreConstants.K8s.TORC_PREFIX}-{self.name}",
+                    "name": texam_name,
+                },
+            ),
+            spec=V1JobSpec(
+                template=V1PodTemplateSpec(
+                    spec=V1PodSpec(
+                        containers=[
+                            V1Container(
+                                name=PoiesisCoreConstants.K8s.TIF_PREFIX,
+                                image=PoiesisCoreConstants.K8s.POIESIS_IMAGE,
+                                command=["texam"],
+                                args=[
+                                    "--name",
+                                    self.name,
+                                    "--executors",
+                                    executors,
+                                    "--resources",
+                                    resources,
+                                    "--volumes",
+                                    volumes,
+                                ],
+                            )
+                        ],
+                        restart_policy="Never",
+                    )
+                )
+            ),
+        )
+        try:
+            await self.kubernetes_client.create_job(job)
+        except ApiException as e:
+            logger.error(e)
+            raise
