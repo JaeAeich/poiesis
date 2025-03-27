07e96c3ee0a4211048b55ce3ed2d965df8b9680b
Javed Habib <jh4official@gmail.com>
2025-03-27 16:23:21 +0530

    feat: add mongo client

--- poiesis/repository/mongo.py
+++ poiesis/repository/mongo.py
@@ -1,14 +1,18 @@
 """MongoDB adaptor for NoSQL database operations."""
 
+import logging
 from contextlib import asynccontextmanager
+from datetime import datetime, timezone
 
 import motor.motor_asyncio
-from pydantic import BaseModel
 
 from poiesis.api.exceptions import DBException
+from poiesis.api.tes.models import TesExecutorLog, TesState, TesTaskLog
 from poiesis.constants import get_poiesis_constants
+from poiesis.repository.schemas import TaskSchema
 
 poiesis_constants = get_poiesis_constants()
+logger = logging.getLogger(__name__)
 
 
 class MongoDBClient:
@@ -32,12 +36,24 @@ class MongoDBClient:
         )
         self.db = self.client[self.database]
 
-    async def insert_one(self, collection: str, document: BaseModel) -> str:
+    async def get_task(self, task_id: str) -> TaskSchema:
+        """Get a task from the database.
+
+        Args:
+            task_id: ID of the task
+        """
+        task = await self.db[
+            poiesis_constants.Database.MongoDB.TASK_COLLECTION
+        ].find_one({"task_id": task_id})
+        if task is None:
+            raise DBException(f"Task with ID {task_id} not found")
+        return TaskSchema(**task)
+
+    async def insert_task(self, task: TaskSchema) -> str:
         """Insert a single document into the specified collection.
 
         Args:
-            collection: Collection name
-            document: Document to insert
+            task: Task to insert
 
         Returns:
             The inserted document ID as a string
@@ -46,11 +62,131 @@ class MongoDBClient:
             DBException: If the document is not valid or the insert operation fails
         """
         try:
-            result = await self.db[collection].insert_one(document.model_dump())
+            result = await self.db[
+                poiesis_constants.Database.MongoDB.TASK_COLLECTION
+            ].insert_one(task.model_dump())
             return str(result.inserted_id)
         except Exception as e:
+            logger.error(
+                "Error inserting document into collection "
+                f"{poiesis_constants.Database.MongoDB.TASK_COLLECTION}: {e.__dict__}"
+            )
             raise DBException(
-                f"Error inserting document into collection {collection}: {e}"
+                "Error inserting document into collection "
+                f"{poiesis_constants.Database.MongoDB.TASK_COLLECTION}: {e}",
+            ) from e
+
+    async def update_task_state(self, task_id: str, state: TesState) -> None:
+        """Update the state of a task in the database.
+
+        This would be called by jobs in case of task state change or failure.
+
+        Args:
+            task_id: ID of the task
+            state: State of the task
+
+        Raises:
+            DBException: If the update operation fails
+        """
+        try:
+            task = await self.get_task(task_id)
+            if task.data.state != state:
+                task.data.state = state
+                await self.db[
+                    poiesis_constants.Database.MongoDB.TASK_COLLECTION
+                ].update_one(
+                    {"task_id": task_id},
+                    {
+                        "$set": {
+                            "state": state.value,
+                            "updated_at": datetime.now(timezone.utc),
+                            "data.state": state.value,
+                        }
+                    },
+                )
+        except Exception as e:
+            logger.error(
+                "Error updating document in collection"
+                f"{poiesis_constants.Database.MongoDB.TASK_COLLECTION}: {e.__dict__}"
+            )
+            raise DBException(
+                "Error updating document in collection"
+                f"{poiesis_constants.Database.MongoDB.TASK_COLLECTION}: {e}",
+            ) from e
+
+    async def add_task_log(self, task_id: str) -> None:
+        """Add a log for a task in the database.
+
+        Args:
+            task_id: ID of the task
+
+        Note:
+            This is because the spec defines that in case of task failure and retry,
+            another log will be added to the task.
+        """
+        _log = TesTaskLog(logs=[], outputs=[])
+        try:
+            task = await self.get_task(task_id)
+            task.data.logs = task.data.logs or []
+            task.data.logs.append(_log)
+            await self.db[
+                poiesis_constants.Database.MongoDB.TASK_COLLECTION
+            ].update_one(
+                {"task_id": task_id},
+                {
+                    "$set": {
+                        "data.logs": [log.model_dump() for log in task.data.logs],
+                    }
+                },
+            )
+        except Exception as e:
+            logger.error(
+                "Error adding task log in collection"
+                f"{poiesis_constants.Database.MongoDB.TASK_COLLECTION}: {e.__dict__}"
+            )
+            raise DBException(
+                "Error adding task log in collection"
+                f"{poiesis_constants.Database.MongoDB.TASK_COLLECTION}: {e}",
+            ) from e
+
+    async def add_task_executor_log(self, task_id: str) -> None:
+        """Append a log for a task in the database.
+
+        Each executor has a log.
+
+        Args:
+            task_id: ID of the task
+
+        Note:
+            This assumes that the executors are called sequentially, hence we will just
+            append to the last log.
+        """
+        # XXX: We initialize the log with exit code 0
+        _log = TesExecutorLog(exit_code=0)
+        try:
+            task = await self.get_task(task_id)
+            # This shouldn't be needed as add_task_log should have been called
+            task.data.logs = task.data.logs or []
+            # Last logs is the current task log, hence we pick the last one
+            task.data.logs[-1].logs.append(_log)
+            await self.db[
+                poiesis_constants.Database.MongoDB.TASK_COLLECTION
+            ].update_one(
+                {"task_id": task_id},
+                {
+                    "$set": {
+                        "data.logs": [log.model_dump() for log in task.data.logs],
+                    }
+                },
+            )
+        except Exception as e:
+            logger.error(
+                "Error upserting task log in collection"
+                f"{poiesis_constants.Database.MongoDB.TASK_COLLECTION}: {e.__dict__}"
+            )
+            raise DBException(
+                "Error upserting task log in collection"
+                f"{poiesis_constants.Database.MongoDB.TASK_COLLECTION}: {e}",
             ) from e
 
     @asynccontextmanager
