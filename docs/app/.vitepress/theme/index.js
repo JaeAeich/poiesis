d257210ba0c461d4468909bbcb9be6bc4b13a03d
Javed Habib <jh4official@gmail.com>
2025-04-21 21:16:03 +0530

    feat(docs): add `vercel` analytics

--- docs/app/.vitepress/theme/index.js
+++ docs/app/.vitepress/theme/index.js
@@ -1,9 +1,15 @@
 import DefaultTheme from 'vitepress/theme-without-fonts';
 import SwaggerViewer from './components/SwaggerViewer.vue';
+import { inject } from '@vercel/analytics'
 import "@catppuccin/vitepress/theme/frappe/blue.css";
 import "./custom.css";
 import "./swagger.css";
 import "./custom-fonts.css";
+
+// Inject Vercel Analytics only in production and on the client-side
+if (process.env.NODE_ENV === 'production' && typeof window !== 'undefined') {
+    inject();
+  }
 
 export default {
   extends: DefaultTheme,
